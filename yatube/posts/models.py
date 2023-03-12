5f408462bae666414b1092ec2d48860cc62bbdab
Wittelsbach-Konig <kiryuschin.vitalij@yandex.ru>
2023-03-12 16:17:53 +0300

    Add final changes

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,11 +5,25 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class Group(models.Model):
+    """Модель групп"""
+    title = models.CharField(max_length=50)
+    slug = models.URLField(max_length=200)
+    description = models.TextField()
+
+
 class Post(models.Model):
+    """Модель постов"""
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
         related_name='posts'
+    )
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
     )
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,21 +1,26 @@
 # from django.shortcuts import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 # Главная страница
 def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
     template = 'posts/index.html'
     context = {
-        'text': 'Это главная страница проекта Yatube'
+        'posts': posts
     }
     return render(request, template, context)
 
 
 # Фильтрация по группам
-def group_posts(request):
+def group_posts(request, slug):
     template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'text': 'Здесь будет информация о группах проекта Yatube'
+        'group': group,
+        'posts': posts,
     }
     return render(request, template, context)
 
